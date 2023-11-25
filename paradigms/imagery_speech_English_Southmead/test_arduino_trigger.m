a0434be77be414488cb39b461a79ad7b77602418
xiaolongwu0713 <xiaolongwu0713@gmail.com>
2023-11-25 16:58:35 +0000

    adding script to test two triggers

---
+++ paradigms/imagery_speech_English_Southmead/test_arduino_trigger.m
@@ -0,0 +1,37 @@
+clear all
+clc
+delete(instrfindall);
+
+%% test the relay 
+arduino=serial('COM7','BaudRate',9600,'DataBits',8);
+InputBufferSize=8;
+Timeout=0.1;
+set(arduino, 'InputBufferSize',InputBufferSize);
+set(arduino, 'Timeout',Timeout);
+set(arduino, 'Terminator','CR');
+
+fopen(arduino);
+
+while 1
+fprintf(arduino,'0');
+pause(3);
+end
+
+%y=fscanf(arduino,'%f')
+fclose(arduino);
+
+%% test the opto isolator
+
+arduino=serial('COM8','BaudRate',9600,'DataBits',8);
+InputBufferSize=8;
+Timeout=0.1;
+set(arduino, 'InputBufferSize',InputBufferSize);
+set(arduino, 'Timeout',Timeout);
+set(arduino, 'Terminator','CR');
+
+fopen(arduino);
+
+while 1
+fprintf(arduino,'0');
+pause(1);
+end
